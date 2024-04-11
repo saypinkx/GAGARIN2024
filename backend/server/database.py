eedc66f86e69d39ed557060cf74c3efe33a5c6a8
sixzerx <paprut538@gmail.com>
2024-04-11 10:13:18 +0300

    edit db

--- backend/server/database.py
+++ backend/server/database.py
@@ -3,8 +3,8 @@ from motor import motor_asyncio
 
 class Database:
     def __init__(self):
-        # self.client = motor_asyncio.AsyncIOMotorClient("mongodb://mongodb:27017")
-        self.client = motor_asyncio.AsyncIOMotorClient("mongodb://localhost:27017")
+        self.client = motor_asyncio.AsyncIOMotorClient("mongodb://mongodb:27017")
+        # self.client = motor_asyncio.AsyncIOMotorClient("mongodb://localhost:27017")
 
         self.database = self.client.gagarin
 
